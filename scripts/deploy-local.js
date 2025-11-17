ed98c8211f1aa5c694cb3171437355a8dee269da
bennyh960 <bennyh960@gmail.com>
2025-11-17 09:22:19 +0200

    fix internal scripts

--- scripts/deploy-local.js
+++ scripts/deploy-local.js
@@ -1,7 +1,7 @@
 // scripts/deploy-local.js
-import fs from 'fs';
-import path from 'path';
-import { execSync } from 'child_process';
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
 
 // 1️⃣ Read package.json to get name and version
 const pkgPath = path.resolve('./package.json');
